b986bca3508be662053cfd4f405f0254e9490bd9
fary19tebo <pariyanto99@admin.sma.belajar.id>
2026-08-06 12:25:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sidimas-cache-v30';
+const CACHE_NAME = 'sidimas-cache-v31';
 
 self.addEventListener('install', (event) => {
   // Langsung update jika ada versi sw.js baru tanpa harus ditutup aplikasinya
